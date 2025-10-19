b2bda39e7a62de8c5246b69922904d370785daf2
Alfredo <alfredo_mariscal1@hotmail.com>
2025-10-18 22:34:36 -0600

    cambios18

--- js/fondo.js
+++ js/fondo.js
@@ -1,35 +1,27 @@
-const botonFondo = document.querySelector("#toggleFondo");
-const cuerpo = document.body;
+const botonFondo = document.querySelector("#toggleFondo"); const cuerpo = document.body; 
 
+window.addEventListener("DOMContentLoaded", () => { 
+cuerpo.classList.add("animated-gradient"); 
+botonFondo.textContent = "Desactivar fondo animado"; 
+}); 
 
-window.addEventListener("DOMContentLoaded", () => {
-  cuerpo.classList.add("animated-gradient");
-  botonFondo.textContent = "Desactivar fondo animado";
+botonFondo.addEventListener("click", () => { 
+cuerpo.classList.toggle("animated-gradient"); 
+
+if (cuerpo.classList.contains("animated-gradient")) { 
+botonFondo.textContent = "Desactivar fondo animado"; 
+} else { botonFondo.textContent = "Activar fondo animado"; 
+} 
+
+botonFondo.classList.add("scale-110"); setTimeout(() => { 
+botonFondo.classList.remove("scale-110"); 
+}, 150); 
+}); 
+
+botonFondo.addEventListener("mouseover", () => { 
+botonFondo.classList.add("shadow-[0_0_15px_#60a5fa]"); 
+}); 
+
+botonFondo.addEventListener("mouseout", () => { 
+botonFondo.classList.remove("shadow-[0_0_15px_#60a5fa]"); 
 });
-
-
-botonFondo.addEventListener("click", () => {
-  cuerpo.classList.toggle("animated-gradient");
-
-  if (cuerpo.classList.contains("animated-gradient")) {
-    botonFondo.textContent = "Desactivar fondo animado";
-  } else {
-    botonFondo.textContent = "Activar fondo animado";
-  }
-
- 
-  botonFondo.classList.add("scale-110");
-  setTimeout(() => {
-    botonFondo.classList.remove("scale-110");
-  }, 150);
-});
-
-
-botonFondo.addEventListener("mouseover", () => {
-  botonFondo.classList.add("shadow-[0_0_15px_#60a5fa]");
-});
-
-botonFondo.addEventListener("mouseout", () => {
-  botonFondo.classList.remove("shadow-[0_0_15px_#60a5fa]");
-});
-
